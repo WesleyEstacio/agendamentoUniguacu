70ee72ee57f35ebb70ec477fe0678edefa08d09f
Wesley Estacio <wesleyestacio4@gmail.com>
2022-04-19 10:58:44 -0300

    feat - Login e Cadastro Layout

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(express.static('./src'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname+'/src/index.html');
 });
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname+'/src/login.html');
+});
+
+app.get('/servicos', (req, res) => {
+    res.sendFile(__dirname+'/src/servico.html');
+});
+
+app.listen(8080)
